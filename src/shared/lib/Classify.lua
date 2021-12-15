115fce2e5abaea5f452a9a6be6c7facfabd78300
doctr-oof <ericmkarolchyk@gmail.com>
2021-12-15 17:12:18 -0500

    Upgrade to Classify 2.0

--- src/shared/lib/Classify.lua
+++ src/shared/lib/Classify.lua
@@ -1,234 +1,262 @@
 --[[
-          _                   _               _  __
-     _ __| |____  __      ___| | __ _ ___ ___(_)/ _|_   _
-    | '__| '_ \ \/ /____ / __| |/ _` / __/ __| | |_| | | |
-    | |  | |_) >  <_____| (__| | (_| \__ \__ \ |  _| |_| |
-    |_|  |_.__/_/\_\     \___|_|\__,_|___/___/_|_|  \__, |
-    Easy-to-use OOP Class Super Constructor         |___/
-    By FriendlyBiscuit/doctr_oof
-    
+          __             _ ___
+     ____/ /__ ____ ___ (_) _/_ __
+    / __/ / _ `(_-<(_-</ / _/ // /
+    \__/_/\_,_/___/___/_/_/ \_, /
+    Classify - OOP Helper  /___/
+    By FriendlyBiscuit
 --]]
 
+--= Root Table =--
+local Classify         = { meta = { }, prototype = { } }
+
 --= Constants =--
-local SUPPRESS_DEBUG = true  -- Suppresses debug messages. Recommended Setting: true. This WILL destroy your output!
-local SUPPRESS_ERROR = false -- Suppresses error messages. Recommended Setting: false.
-local STRINGS        = {     -- Output message templates. Recommend you don't modify these.
-    NO_CLASS_NAME = 'Failed to classify table - "__classname" must be defined.';
-    NOREAD_PROPERTY = 'Failed to get property "%s" of "%s" - property cannot be read.';
+local SUPPRESS_ERROR   = false
+local MESSAGES         = {
+    NO_CLASS_NAME      = 'Failed to classify table - "__classname" must be defined.';
     READONLY_PROPERTY = 'Failed to set property "%s" of "%s" - property is read-only.';
-    DESTROYED = 'Attempted to reference a nil class.';
-    ZERO_DISPOSABLES = 'Cannot :add_disposables() with zero items provided.';
-    CLASSIFYING = 'Classifying "%s"...';
-    INDEX_ORIGINAL = 'Using original __index function for "%s" for member "%s"';
-    INDEX_NEW = 'Using new __index function for "%s" for member "%s"';
-    NEWINDEX_ORIGINAL = 'Using original __newindex function for "%s" for member "%s" with value %q';
-    NEWINDEX_NEW = 'Using new __newindex function for "%s" for member "%s" with value %q';
+    ZERO_DISPOSABLES = 'Cannot :_mark_disposables() with zero items provided.';
+    INVALID_CLASS = '%q cannot inherit  %q - target dependency is not a valid Classify class.';
+    INDEX_ALREADY_EXISTS = '%q cannot inherit %q - %q already exists in the inheritor.';
+    PROPERTY_ALREADY_EXISTS = 'Duplicate inherited property %q from %q has been overwritten in %q.';
 }
 
---= Utility Functions =--
-function debug(message: string, ...)
-    if SUPPRESS_DEBUG then return end
-    
-    local output = ('[classify-debug] %s'):format(message)
-    
-    if #{...} > 0 then
-        output = output:format(...)
-    end
-    
-    print(output)
-end
-
-function err(message: string, ...)
+--= Internal Functions =--
+function wrn(message: string, ...): nil
     if SUPPRESS_ERROR then return end
     
-    local output = ('[classify-error] %s'):format(message)
-    
     if #{...} > 0 then
-        output = output:format(...)
+        message = message:format(...)
     end
     
-    error(output, 3)
+    warn(message)
 end
 
-function deep_copy(input: table)
+function err(message: string, ...): nil
+    if SUPPRESS_ERROR then return end
+    
+    if #{...} > 0 then
+        message = message:format(...)
+    end
+    
+    error(message, 3)
+end
+
+function deep_copy(source: table, target: table): table|nil
     local result = { }
     
-    if type(input) == 'table' then
-        for index, value in next, input, nil do
+    if type(source) == 'table' then
+        for index, value in next, source, nil do
+            --print('Copying', index, value)
             rawset(result, deep_copy(index), deep_copy(value))
         end
         
-        setmetatable(result, deep_copy(getmetatable(input)))
+        setmetatable(result, deep_copy(getmetatable(source)))
     else
-        result = input
+        result = source
     end
     
-    return result
+    if target then
+        for index, value in next, source, nil do
+            rawset(target, index, value)
+        end
+    else
+        return result
+    end
 end
 
---= Classify =--
-function classify(class: table)
+--= Meta-Functions =--
+function Classify.meta.__index(self: table, key: string): any
+    local properties = rawget(self, '__properties')
+    
+    if key == 'ClassName' then
+        return rawget(self, '__classname')
+    end
+    
+    if properties then
+        local property = properties[key]
+        
+        if property then
+            if property.get then
+                return property.get(self)
+            elseif property.bind and property.target then
+                return property.target(self)[property.bind]
+            else
+                err(MESSAGES.NOREAD_PROPERTY, key, rawget(self, '__classname'))
+            end
+        end
+    end
+    
+    return rawget(self, key)
+end
+
+function Classify.meta.__newindex(self: table, key: string, value: any): nil
+    local properties = rawget(self, '__properties')
+    local success = false
+    
+    if key == '__cleaning' and type(value) == 'function' then
+        rawget(self, '__meta').clean_callback = value
+        return
+    end
+    
+    if properties then
+        local property = properties[key]
+        
+        if property then
+            if property.set then
+                property.set(self, value)
+                success = true
+            end
+            
+            if property.bind and property.target then
+                property.target(self)[property.bind] = value
+                success = true
+            end
+            
+            if not property.bind and not property.set then
+                err(MESSAGES.READONLY, key, rawget(self, '__classname'))
+            end
+        end
+    end
+    
+    if not success then
+        rawset(self, key, value)
+    end
+end
+
+function Classify.meta.__tostring(self: table): string
+    return rawget(self, 'Name') or rawget(self, '__classname')
+end
+
+--= Prototype/Injected Functions =--
+function Classify.prototype:_mark_disposable(trash: any): nil
+    table.insert(rawget(self, '__meta').disposables, trash)
+end
+
+function Classify.prototype:_mark_disposables(...): nil
+    if #{...} > 0 then
+        for _, item in pairs(...) do
+            table.insert(rawget(self, '__meta').cleanup, item)
+        end
+    else
+        err(MESSAGES.ZERO_DISPOSABLES)
+    end
+end
+
+function Classify.prototype:_dispose(): nil
+    local disposables = rawget(self, '__meta').disposables
+    local index, item = next(disposables)
+    
+    while item ~= nil do
+        disposables[index] = nil
+        
+        if typeof(item) == 'RBXScriptConnection' then
+            item:Disconnect()
+        elseif type(item) == 'function' then
+            item()
+        elseif item.Destroy then
+            item:Destroy()
+        end
+        
+        index, item = next(disposables)
+    end
+end
+
+function Classify.prototype:_clean(): nil
+    for index, _ in pairs(self) do
+        rawset(self, index, nil)
+    end
+    
+    setmetatable(self, {
+        __index = function() return nil end,
+        __newindex = function() return nil end,
+        __tostring = function() return nil end,
+        __metatable = { }
+    })
+end
+
+function Classify.prototype:Inherit(dependency: any): table
+    local copy = deep_copy(dependency)
+    local d_class_name = rawget(copy, '__classname')
+    local s_class_name = rawget(self, '__classname')
+    
+    if not d_class_name then
+        err(MESSAGES.INVALID_CLASS, s_class_name, d_class_name)
+        return
+    end
+    
+    for index, value in next, copy, nil do
+        local raw = rawget(copy, index)
+        
+        if index == '__index' then
+            table.insert(rawget(self, '__meta').d_index, value)
+        elseif index == '__newindex' then
+            table.insert(rawget(self, '__meta').d_newindex, value)
+        elseif index == '__meta' then
+            for _, callback in pairs(raw.clean_callbacks) do
+                table.insert(rawget(self, '__meta').clean_callbacks, callback)
+            end
+        elseif index == '__properties' then
+            local d_properties = rawget(copy, '__properties')
+            local s_properties = rawget(self, '__properties')
+            
+            if d_properties and s_properties then
+                for property, data in pairs(d_properties) do
+                    if s_properties[property] ~= nil then
+                        wrn(MESSAGES.PROPERTY_ALREADY_EXISTS, property, d_class_name, s_class_name)
+                    end
+                    
+                    s_properties[property] = data
+                end
+            end
+        else
+            if rawget(self, index) then
+                continue
+            end
+            
+            rawset(self, index, raw)
+        end
+    end
+    
+    return self
+end
+
+function Classify.prototype:Destroy(...): nil
+    local clean_callbacks = rawget(self, '__meta').clean_callbacks
+    
+    for _, callback in pairs(clean_callbacks) do
+        callback(...)
+    end
+    
+    self:_dispose()
+    self:_clean()
+end
+
+Classify.prototype.inherit = Classify.prototype.Inherit
+
+--= Main Module Function =--
+function Classify.classify(class: table): any
     local class_name = rawget(class, '__classname')
     
     if class_name == nil then
-        err(STRINGS.NO_CLASS_NAME)
+        err(MESSAGES.NO_CLASS_NAME)
         return
     end
     
     local proxy, result = deep_copy(class)
     
+    deep_copy(Classify.meta, proxy)
+    deep_copy(Classify.prototype, proxy)
+    
     rawset(proxy, '__meta', {
-        index = rawget(proxy, '__index'),
-        newindex = rawget(proxy, '__newindex'),
-        destroyed = rawget(proxy, '__cleaning'),
-        cleanup = { }
+        clean_callbacks = { rawget(proxy, '__cleaning') },
+        d_index = { },
+        d_newindex = { },
+        disposables = { }
     })
-    
-    proxy.__index = function(self, key)
-        if key == 'ClassName' then
-            return rawget(self, '__classname')
-        end
-        
-        if rawget(self, '__properties') then
-            local property = rawget(self, '__properties')[key]
-            
-            if property then
-                if property.get then
-                    return property.get(self)
-                elseif property.bind and property.target then
-                    return property.target(self)[property.bind]
-                else
-                    err(STRINGS.NOREAD_PROPERTY, key, class_name)
-                end
-            end
-        end
-        
-        local meta = rawget(self, '__meta').index
-        
-        if meta then
-            debug(STRINGS.INDEX_ORIGINAL, class_name, key)
-            return meta(self, key)
-        else
-            debug(STRINGS.INDEX_NEW, class_name, key)
-            return rawget(self, key)
-        end
-    end
-    
-    proxy.__newindex = function(self, key, value)
-        local prop = false
-        
-        if key == '__cleaning' and type(value) == 'function' then
-            rawget(self, '__meta').destroyed = value
-        end
-        
-        if rawget(self, '__properties') then
-            local property = rawget(self, '__properties')[key]
-            
-            if property then
-                prop = true
-                
-                if property.set then
-                    property.set(self, value)
-                elseif property.bind and property.target then
-                    property.target(self)[property.bind] = value
-                else
-                    err(STRINGS.READONLY_PROPERTY, key, class_name)
-                end
-            end
-        end
-        
-        local meta = rawget(self, '__meta').newindex
-        
-        if meta then
-            debug(STRINGS.NEWINDEX_ORIGINAL, class_name, key, value)
-            meta(self, key, value)
-        elseif not prop then
-            rawset(self, key, value)
-        end
-    end
-    
-    proxy.__tostring = function(self)
-        if rawget(self, 'Name') then
-            return rawget(self, 'Name')
-        else
-            return rawget(self, '__classname')
-        end
-    end
-    
-    proxy.Destroy = function(self, ...)
-        if rawget(self, '__meta').destroyed then
-            rawget(self, '__meta').destroyed(self, ...)
-        end
-        
-        self:_dispose()
-        self:_clean()
-    end
-    
-    proxy._dispose = function(self)
-        local disposables = rawget(self, '__meta').cleanup
-        local index, item = next(disposables)
-        
-        while item ~= nil do
-            disposables[index] = nil
-            
-            if typeof(item) == 'RBXScriptConnection' then
-                item:Disconnect()
-            elseif type(item) == 'function' then
-                item()
-            elseif item.Destroy then
-                item:Destroy()
-            end
-            
-            index, item = next(disposables)
-        end
-    end
-    
-    proxy._clean = function(self)
-        for index, _ in pairs(self) do
-            rawset(self, index, nil)
-        end
-        
-        setmetatable(self, {
-            __index = function() return nil end,
-            __newindex = function() return nil end,
-            __tostring = function() return nil end,
-            __metatable = { }
-        })
-        
-        --[[setmetatable(self, {
-            __index = function() err(STRINGS.DESTROYED) end,
-            __newindex = function() err(STRINGS.DESTROYED) end,
-            __tostring = function() err(STRINGS.DESTROYED) end,
-            __metatable = { }
-        })--]]
-        
-        class_name = nil
-        proxy = nil
-        result = nil
-    end
-    
-    proxy._mark_disposable = function(self, item: any)
-        table.insert(rawget(self, '__meta').cleanup, item)
-    end
-    
-    proxy._mark_disposables = function(self, ...)
-        if #{...} > 0 then
-            for _, item in pairs(...) do
-                table.insert(rawget(self, '__meta').cleanup, item)
-            end
-        else
-            err(STRINGS.ZERO_DISPOSABLES)
-        end
-    end
-    
-    proxy._markDisposable = proxy._mark_disposable
-    proxy._markDisposables = proxy._mark_disposables
-    proxy._MarkDisposable = proxy._mark_disposable
-    proxy._MarkDisposables = proxy._mark_disposables
     
     result = deep_copy(proxy)
     return setmetatable(result, proxy)
 end
 
---= Return Classify =--
-return classify
+return Classify.classify
