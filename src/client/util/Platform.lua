6902d97a7d6b2a29860ff1cf5108a12f5cd03406
doctr-oof <ericmkarolchyk@gmail.com>
2022-01-18 13:24:54 -0500

    NEW documented Platform client utility

--- src/client/util/Platform.lua
+++ src/client/util/Platform.lua
@@ -1,14 +1,42 @@
+--[[
+       ___  __     __  ___
+      / _ \/ /__ _/ /_/ _/__  ______ _
+     / ___/ / _ `/ __/ _/ _ \/ __/  ' \
+    /_/  /_/\_,_/\__/_/ \___/_/ /_/_/_/
+    FriendlyBiscuit
+    01/18/2022 @ 13:18:22
+    
+    Description:
+        Provides a quickly-accessible collection of flags that expose platform information to the client.
+    
+    Documentation:
+        Flags:
+            boolean IS_CONSOLE
+            -> Returns whether or not the client is playing on a genuine console device.
+            
+            boolean IS_MOBILE
+            -> Returns whether or not the client is playing on a genuine mobile device (emulator included).
+            
+            boolean IS_PC
+            -> Returns true if IS_CONSOLE and IS_MOBILE are both false.
+--]]
+
+--= Module Root =--
+local module    = { }
+
+--= Roblox Services =--
 local input_svc = game:GetService('UserInputService')
 local gui_svc   = game:GetService('GuiService')
-local IS_CONSOLE = gui_svc:IsTenFootInterface()
-local IS_MOBILE = input_svc.TouchEnabled
-    and not input_svc.KeyboardEnabled
-    and not input_svc.MouseEnabled and not input_svc.GamepadEnabled
-    and not gui_svc:IsTenFootInterface()
-local IS_PC = not IS_CONSOLE and not IS_MOBILE
 
-return {
-    IS_CONSOLE = IS_CONSOLE,
-    IS_MOBILE = IS_MOBILE,
-    IS_PC = IS_PC
-}
+--= Flags =--
+module.IS_CONSOLE = gui_svc:IsTenFootInterface()
+
+module.IS_MOBILE = input_svc.TouchEnabled
+                   and not input_svc.KeyboardEnabled
+                   and not input_svc.MouseEnabled and not input_svc.GamepadEnabled
+                   and not gui_svc:IsTenFootInterface()
+
+module.IS_PC = not module.IS_CONSOLE and not module.IS_MOBILE
+
+--= Return Module =--
+return module
